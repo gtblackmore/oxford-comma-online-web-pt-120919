01c83e8d806e41c3a7414b59d5856b426818f033
George Blackmore <gtblackmore@gmail.com>
2019-12-13 04:22:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
   if array.length < 2
-    array.join
+    return array.join
   elsif array.length == 2
-    array.join(" and ")
-  else array.join(", ")
+    return array.join(" and ")
+  else 
+    return array[0..-2].join(", ") +", and " + array[-1]
   end
 end
